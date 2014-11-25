ff147253e190ac04276959362b7dc5440964f5c1
monjohn <johnston.monte@gmail.com>
2014-11-24 23:32:20 -0500

    deleted lein-ring

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject clojureapp "0.1.0-SNAPSHOT"
-    :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
-    :main webapp)
+(defproject temperature "0.1.0-SNAPSHOT"
+  :description "a place to track temperature over time"
+  :url "http://temperature-monjohn.rhcloud.com/"
+    :plugins [;[lein-ring "0.7.5"]
+              ]
+    :dependencies [[org.clojure/clojure "1.6"]
+                   [http-kit "2.1.16"]]
+    :target-path "target/%s"
+    :main ^:skip-aot webapp)
