ae4546989529938c12b374f3df5678d10b984e3e
monjohn <johnston.monte@gmail.com>
2015-02-13 14:13:46 -0500

    added full url

--- src/speicher/client.cljs
+++ src/speicher/client.cljs
@@ -16,7 +16,7 @@
 
 (defn fetch-list [state kw]
   (go (let [ch (:input-chan state)
-            response (<! (http/get (str "/list/" kw) ;{:edn-params {:list kw}}
+            response (<! (http/get (str "http://http://temperature-monjohn.rhcloud.com/list/" kw) ;{:edn-params {:list kw}}
                                    ))]
         (>! ch [:response response]))))
 
