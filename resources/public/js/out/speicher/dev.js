ec10bc682af65548bcbf67918b97e953645d66b6
monjohn <johnston.monte@gmail.com>
2015-02-18 07:59:27 -0500

    removed build files

--- resources/public/js/out/speicher/dev.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 0.0-2850 {}
-goog.provide('speicher.dev');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('speicher.client');
-speicher.client.main.call(null);
